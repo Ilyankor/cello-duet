c580180f91f46fe74f82283f735d5969efd84474
miles bi <46078110+Ilyankor@users.noreply.github.com>
2021-03-20 15:44:59 -0500

    Update pr-tr.py

--- pr-tr.py
+++ pr-tr.py
@@ -12,15 +12,15 @@ tr = df.to_numpy()
 
 #define the initial vector v_i
 if start == "D":
-    vi = ([1, 0, 0, 0, 0])
+    v0 = ([1, 0, 0, 0, 0])
 elif start == "E":
-    vi = ([0, 1, 0, 0, 0])
+    v0 = ([0, 1, 0, 0, 0])
 elif start == "F":
-    vi = ([0, 0, 1, 0, 0])
+    v0 = ([0, 0, 1, 0, 0])
 elif start == "A":
-    vi = ([0, 0, 0, 1, 0])
+    v0 = ([0, 0, 0, 1, 0])
 elif start == "B":
-    vi = ([0, 0, 0, 0, 1])
+    v0 = ([0, 0, 0, 0, 1])
 
 #the markov process
 tr2 = np.dot(tr, tr)
@@ -31,14 +31,14 @@ pitches = ['D', 'E', 'F#', 'A', 'B']
 
 #function
 if num == '2':
-    output = np.random.choice(pitches, 1, p=np.dot(tr, vi))
+    output = np.random.choice(pitches, 1, p=np.dot(tr, v0))
     print(output)
 elif num == '3':
-    output1 = np.random.choice(pitches, 1, p=np.dot(tr, vi))
-    output2 = np.random.choice(pitches, 1, p=np.dot(tr2, vi))
+    output1 = np.random.choice(pitches, 1, p=np.dot(tr, v0))
+    output2 = np.random.choice(pitches, 1, p=np.dot(tr2, v0))
     print(output1, output2)
 elif num == '4':
-    output1 = np.random.choice(pitches, 1, p=np.dot(tr, vi))
-    output2 = np.random.choice(pitches, 1, p=np.dot(tr2, vi))
-    output3 = np.random.choice(pitches, 1, p=np.dot(tr3, vi))
+    output1 = np.random.choice(pitches, 1, p=np.dot(tr, v0))
+    output2 = np.random.choice(pitches, 1, p=np.dot(tr2, v0))
+    output3 = np.random.choice(pitches, 1, p=np.dot(tr3, v0))
     print(output1, output2, output3)
