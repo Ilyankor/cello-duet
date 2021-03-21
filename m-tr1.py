f7f4477d80e15c5ae9c4fdf9a154014695b54cd1
miles bi <46078110+Ilyankor@users.noreply.github.com>
2021-03-20 19:02:27 -0500

    Update and rename tes.py to m-tr1.py

--- m-tr1.py
+++ m-tr1.py
@@ -2,14 +2,18 @@ import numpy as np
 from pandas import DataFrame, read_excel
 from numpy.linalg import matrix_power
 
+#import transition matrix
 df = read_excel("master.xls", sheet_name="m-tr1", header=None)
 tr = df.to_numpy()
 
+#create random initial vector
 v = np.random.rand(24)
 v0 = v/np.sum(v)
 
+#create 24 options for T, TnI
 transform = np.arange(24)
 
+#function
 for i in range(1, 15):
     out = np.random.choice(transform, 1, p=np.dot(matrix_power(tr, i), v0))
     print(out)
